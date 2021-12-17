6a8b5cbb7ba8be39fc66d3eeaa4781eeb410d566
dlss84 <Dlss84@gmail.com>
2021-12-16 19:06:30 -0500

    icons - app

--- app/src/main/java/com/sebastiani/tripsmountain/DiscoverFragment.java
+++ app/src/main/java/com/sebastiani/tripsmountain/DiscoverFragment.java
@@ -1,6 +1,18 @@
 package com.sebastiani.tripsmountain;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 public class DiscoverFragment extends Fragment {
+
+    @Override
+    public View onCreateView(
+            @NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        return inflater.inflate(R.layout.discover_fragment,container,false);
+    }
 }
